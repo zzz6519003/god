34178b5bdf715bfaac33140959d384b0587c1ae4
Zhengzhong <zhengzhongzhao@gmail.com>
2015-01-12 23:54:49 +0800

    style

--- config/middlewares/authorization.js
+++ config/middlewares/authorization.js
@@ -3,8 +3,10 @@
  *  Generic require login routing middleware
  */
 
-exports.requiresLogin = function (req, res, next) {
-  if (req.isAuthenticated()) return next()
+exports.requiresLogin = function(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
   if (req.method == 'GET') req.session.returnTo = req.originalUrl
   res.redirect('/login')
 }
@@ -14,7 +16,7 @@ exports.requiresLogin = function (req, res, next) {
  */
 
 exports.user = {
-  hasAuthorization: function (req, res, next) {
+  hasAuthorization: function(req, res, next) {
     if (req.profile.id != req.user.id) {
       req.flash('info', 'You are not authorized')
       return res.redirect('/users/' + req.profile.id)
